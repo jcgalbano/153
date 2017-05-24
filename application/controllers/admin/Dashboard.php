c0401b9b54a3179ad41e3252cc90788ed63c41db
DarkPotatoKing <kyle.rosales97@gmail.com>
2017-05-25 00:27:38 +0800

    Rename dashboard.php to Dashboard.php

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -1,14 +1,14 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
- 
+
 class Dashboard extends Admin_Controller
 {
-	 
+
 	  function __construct()
 	  {
 	      parent::__construct();
 	  }
 
-	  // simply renders the dashboard_view under the admin folder in the views  
+	  // simply renders the dashboard_view under the admin folder in the views
 	  public function index()
 	  {
 	      $this->render('admin/dashboard_view');
